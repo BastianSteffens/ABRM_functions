952165c375f7c6519a620d0e06f050a09178e05a
Bastian Steffens <bs48@WIN.PET.HW.AC.UK>
2021-02-10 20:28:03 +0000

    split up voronoi into 2 functions, speeded up, variations on how many agents can launched every n turns, max agents also availabe.

--- abm/Model.py
+++ abm/Model.py
@@ -12,6 +12,7 @@ from shapely.geometry import Point
 from shapely.geometry import Polygon
 from geovoronoi import voronoi_regions_from_coords
 from geovoronoi import polygon_lines_from_voronoi
+from collections import Counter
 
 from Agent import Agent
 from Grid import Grid
@@ -37,7 +38,7 @@ class Model():
 
     def __init__(self, 
                 env, 
-                number_of_turns=2000,number_of_starting_agents = 10,new_agent_every_n_turns = 1,max_number_agents = 10,
+                number_of_turns=2000,number_of_starting_agents = 10,new_agent_every_n_turns = 1,new_agents_per_n_turns = 1,max_number_agents = 10,
                 ratio_of_tracked_agents = 1.,number_training_image_zones = 2, number_training_images_per_zone = 20,
                 move_preference_matrix=preference_mat, 
                 stochastic_move_matrix=stochastic_mat, 
@@ -51,25 +52,24 @@ class Model():
         self.training_images = np.linspace((1,1),(number_training_images_per_zone,number_training_images_per_zone),number_training_images_per_zone)
 
         self.initiate_grid()
+        self.init_TI_zone_grid()
 
         self.istep = 0
         self.current_id = -1
 
         self.number_of_turns = number_of_turns
         self.new_agent_every_n_turns = new_agent_every_n_turns
+        self.new_agents_per_n_turns = new_agents_per_n_turns
+        self.number_of_starting_agents = number_of_starting_agents
+        self.max_number_agents = max_number_agents
         self.when_new_agent = 0
 
-        self.number_of_starting_agents = number_of_starting_agents
 
-        # self.new_agents_per_turn = new_agents_per_turn
-        # self.bottom_agent_ratio_birth = bottom_agent_ratio_birth
         self.ratio_of_tracked_agents = ratio_of_tracked_agents
 
         self.move_preference_matrix = move_preference_matrix
         self.stochastic_move_matrix = stochastic_move_matrix
 
-        # self.active_agents = set()
-        # self.dead_agents = set()
         self.active_agents = []
         self.dead_agents = []
         
@@ -110,8 +110,8 @@ class Model():
                 #create not tracked agent
                 agent = Agent(agentID, StartPos, self.istep, isTracked=False)
         self.grid.initiate_agent_on_grid(agent)
-        # self.active_agents.add(agent)
         self.active_agents.append(agent)
+        print("generated new agent")
         # print('Generating agent {}/{}'.format(inew_agent+1, self.new_agents_per_turn), end="\r")
 
     def kill_agent(self, agent, next_turn=False):
@@ -120,7 +120,6 @@ class Model():
         else:
             agent.kill(self.istep)
         self.active_agents.discard(agent)
-        # self.dead_agents.add(agent)
         self.dead_agents.append(agent)
     
     def move_agents(self):
@@ -128,7 +127,7 @@ class Model():
         # n_agents = len(copy_active_agents)
 
         for iagent, agent in enumerate (copy_active_agents):
-            print("moving agent {}".format(iagent))
+            print("moving agent {}".format(iagent), end="\r")
             new_pos = self.get_agent_movement(agent)
             if type(new_pos) == int:
                 if new_pos == 5:
@@ -191,10 +190,13 @@ class Model():
         self.istep += 1
 
         if self.new_agent_every_n_turns == self.when_new_agent:
-            # if self.istep % self.new_agent_every_n_turns == 0:
-            self.generate_new_agent()
-            print("generating new agent")
-            self.when_new_agent = 0
+            if len(self.active_agents)< self.max_number_agents:
+
+                for i in range(self.new_agents_per_n_turns):
+                    self.generate_new_agent()
+                self.when_new_agent = 0
+            else:
+                print("max number of agents generated - wait till agent dies")
 
         self.move_agents()
         print('Moving agents done                  ')
@@ -219,7 +221,6 @@ class Model():
         "track position etc of each agent at end of each iteration"
         for agent in self.active_agents:
             agent.track_agent()
-
 
     def get_all_tracks(self):
         """ Extract the tracking path of all the agents during the simulation in numpy.array of shape (number of agent, number of turns, 3 coordinates) """
@@ -275,9 +276,39 @@ class Model():
         # self.calculate_misfit()
         self.track_agents()
     
+    def init_TI_zone_grid(self):
+        """ Grid that is later used to assign each point of model to a polygon from vornoi tesselation"""
+        # #define grid and  position initianinon points of n polygons
+
+        self.TI_grid = Polygon([(0, 0), (0, self.Y), (self.X, self.Y), (self.X, 0)])
+
+        # generate 2D mesh
+        x = np.arange(0,self.X+1,1,)
+        y = np.arange(0,self.Y+1,1)
+        x_grid, y_grid = np.meshgrid(x,y)
+
+        #get cell centers of mesh
+        x_cell_center = x_grid[:-1,:-1]+0.5
+        y_cell_center = y_grid[:-1,:-1]+0.5
+
+        cell_center_which_polygon = np.zeros(len(x_cell_center.flatten()))
+
+        # array to assign polygon id [ last column] to cell id [first 2 columns]
+        self.TI_grid_all_cell_center = np.column_stack((x_cell_center.flatten(),y_cell_center.flatten(),cell_center_which_polygon))
+
+        #shapely points of each point of the TI_grid
+        self.TI_grid_points = [Point(np.array([self.TI_grid_all_cell_center[i,0],self.TI_grid_all_cell_center[i,1]])) for i in range(len(self.TI_grid_all_cell_center))]
+        #index of each shapely point
+        self.index_by_id = dict((id(points), i) for i, points in enumerate(self.TI_grid_points))
+        #list of TI_grid_points shapely
+        self.list_TI_grid_points = []     
+        for i in range(len(self.TI_grid_points)):
+            self.list_TI_grid_points.append(self.index_by_id[id(self.TI_grid_points[i])])
+        #speed up iteration through points in get_voronoi_regions function
+        self.tree_TI_grid_points = STRtree(self.TI_grid_points)
+
     def generate_voronoi_regions(self):
         """ split up model into voronoi regoins, with each agent marking the center of a voronoi region"""
-
         voronoi_x = []
         voronoi_y = []
         voronoi_z = []
@@ -294,46 +325,27 @@ class Model():
         voronoi_points = np.vstack((voronoi_x,voronoi_y)).T
         # voronoi_z = np.array(voronoi_z)
 
-        # #define grid and  position initianinon points of n polygons
-        grid = Polygon([(0, 0), (0, self.Y), (self.X, self.Y), (self.X, 0)])
-
-        # generate 2D mesh
-        x = np.arange(0,self.X+1,1,)
-        y = np.arange(0,self.Y+1,1)
-        x_grid, y_grid = np.meshgrid(x,y)
-
-        #get cell centers of mesh
-        x_cell_center = x_grid[:-1,:-1]+0.5
-        y_cell_center = y_grid[:-1,:-1]+0.5
-
-        cell_center_which_polygon = np.zeros(len(x_cell_center.flatten()))
-
-        # array to assign polygon id [ last column] to cell id [first 2 columns]
-        all_cell_center = np.column_stack((x_cell_center.flatten(),y_cell_center.flatten(),cell_center_which_polygon))
-
         # get voronoi regions
-        poly_shapes, pts, poly_to_pt_assignments = voronoi_regions_from_coords(voronoi_points, grid,farpoints_max_extend_factor = 30)
-        
-        # assign polygons to TI_zones that they hold the majority of
-        grid_points = [Point(np.array([all_cell_center[i,0],all_cell_center[i,1]])) for i in range(len(all_cell_center))]
+        poly_shapes, pts, poly_to_pt_assignments = voronoi_regions_from_coords(voronoi_points, self.TI_grid,farpoints_max_extend_factor = 30)
 
-        tree_grid_points = STRtree(grid_points)
-        index_by_id = dict((id(points), i) for i, points in enumerate(grid_points))
+        # figure out which points of grid lie in which polygon
         points_in_polygon = []
 
         for voronoi_polygon_id in range(len(poly_shapes)):
-            points_in_polygon.append([[point.x, point.y,index_by_id[id(point)]] for point in tree_grid_points.query(poly_shapes[voronoi_polygon_id]) if point.intersects(poly_shapes[voronoi_polygon_id])])
-        # if points not within polygon due to vertices problem of shapely (point lies on vertices --> point undetected)
+            points_in_polygon.append([[point.x, point.y,self.index_by_id[id(point)]] for point in self.tree_TI_grid_points.query(poly_shapes[voronoi_polygon_id]) if point.intersects(poly_shapes[voronoi_polygon_id])])
+       
+       # if points not within polygon due to vertices problem of shapely (point lies on vertices --> point undetected)
         points_in_polygon_temp = [item for sublist in points_in_polygon for item in sublist]
         detected_points = [row[2] for row in points_in_polygon_temp]
-        all_points = []
-        for i in range(len(grid_points)):
-            all_points.append(index_by_id[id(grid_points[i])])
-        missed_points =list(set(all_points) - set(detected_points))
+
+        missed_points =list(set(self.list_TI_grid_points) - set(detected_points))
+
         shift = 1e-9
-        while len(missed_points)>0:
+        while len(missed_points)!=0:
+            # print("missing points while loop")
+            # print(missed_points)
             for points in missed_points:
-                point = grid_points[points]
+                point = self.TI_grid_points[points]
                 point_shifted = Point(point.x+shift,point.y+shift)
                 for voronoi_polygon_id in range(len(poly_shapes)):
                     if point_shifted.within(poly_shapes[voronoi_polygon_id]):
@@ -341,66 +353,135 @@ class Model():
                         points_in_polygon[voronoi_polygon_id].append(point_stats)
             points_in_polygon_temp = [item for sublist in points_in_polygon for item in sublist]
             detected_points = [row[2] for row in points_in_polygon_temp]
-            all_points = []
-            for i in range(len(grid_points)):
-                all_points.append(index_by_id[id(grid_points[i])])
-            missed_points =list(set(all_points) - set(detected_points))
-            shift += 1e-5
 
+            missed_points =list(set(self.list_TI_grid_points) - set(detected_points))
+            shift += 1e-5 
+
+        #id that every polygon has that points belong to. position 4
         polygon_id = []
-
         for i in range(len(poly_shapes)):
             polygon_id.extend([i]*len(points_in_polygon[i]))
-
         points_in_polygon = [item for sublist in points_in_polygon for item in sublist]
-
         for i in range(len(points_in_polygon)):
             points_in_polygon[i].append(polygon_id[i])
-            
-        df_points_in_polygon = pd.DataFrame(points_in_polygon,columns=["point_x","point_y","point_id","polygon_id"])
-        # df_points_in_polygon = df_points_in_polygon.reset_index(drop = True)
-        df_points_in_polygon.drop_duplicates(subset ="point_id", keep = "last", inplace = True)
 
-        # # if point not assigend to polygon
-        # missing_points = self.missing_elements(list(df_points_in_polygon.point_id))
+        # assign index to list. need this later. position 5
+        for i in range(len(points_in_polygon)):
+            points_in_polygon[i].append(i)
 
-        # if len(missing_points) > 0:
-        #     for i in missing_points:
-        #         missing_point_x = grid_points[i].x
-        #         missing_point_y = grid_points[i].y
-        #         point_id = i
-        #         point_id_next = i+1
-                
-        #         polygon_id = df_points_in_polygon[df_points_in_polygon.point_id == point_id_next].polygon_id.to_list()
-        #         df_missing_point = pd.DataFrame([[missing_point_x,missing_point_y,point_id,polygon_id[0]]], columns = ["point_x","point_y","point_id","polygon_id"])
-        #         df_points_in_polygon = df_points_in_polygon.append(df_missing_point,ignore_index=True)
+        # some points are assigned to multiple polygons. dropping dublicate point ids
+        points_in_polygon_no_dublicates = []
+        points_in_polygon_no_dublicates_set  = set()
+        for point in points_in_polygon:
+            point_id = tuple(point)[2]
+            if point_id not in points_in_polygon_no_dublicates_set:
+                points_in_polygon_no_dublicates.append(point)
+                points_in_polygon_no_dublicates_set.add(point_id)
 
-        df_points_in_polygon = df_points_in_polygon.sort_values(by = ["point_id"])
-        df_points_in_polygon["TI_zone"] = self.env.flatten()
+        #sort list by point_id
+        points_in_polygon_no_dublicates.sort(key=lambda x: x[2])
 
+        #if still not all points found, somethings is wrong. current fix: append last point to list again
+        while len(points_in_polygon_no_dublicates)!= len(self.env.flatten()):
+            print("problem with polygon to point assignemnt. temporary fix")
+            points_in_polygon_no_dublicates.append(points_in_polygon_no_dublicates[-1])
+            print(len(points_in_polygon_no_dublicates))
 
-        df_points_in_polygon = df_points_in_polygon.sort_index()
+        #append TI_zone that each point belongs to to list. position 6
+        TI_zones = self.env.flatten()
+        for i in range(len(points_in_polygon_no_dublicates)):
+            points_in_polygon_no_dublicates[i].append(TI_zones[i])
 
-        TI_zone_value_all = []
+        #sort list by index
+        points_in_polygon_no_dublicates.sort(key=lambda x: x[3])
+
+        # what TI_zone dominates in which polygon
+        TI_zone_most_common_all =  []
         for voronoi_polygon_id in range(len(poly_shapes)):
-            TI_zone_value = df_points_in_polygon[df_points_in_polygon.polygon_id == voronoi_polygon_id]["TI_zone"].value_counts(dropna = False).index
-            if len(TI_zone_value) == 0:
-                TI_zone_value = TI_zone_value_all[-1]
+            TI_counter_per_polygon = []
+            for point in points_in_polygon_no_dublicates:
+                polygon_id = point[3]
+                if polygon_id == voronoi_polygon_id:
+                    TI_counter_per_polygon.append(point[5])
+            if not TI_counter_per_polygon:
+                print("penis")
+                TI_most_common = np.random.randint(len(poly_shapes)+1)
             else:
-                TI_zone_value = TI_zone_value[0].tolist()
+                TI_most_common = Counter(TI_counter_per_polygon).most_common(1)[0][0]
+      
+            TI_zone_most_common_all.extend([TI_most_common]*len(TI_counter_per_polygon))
 
-            TI_zone_value_all.extend([TI_zone_value]*len(df_points_in_polygon[df_points_in_polygon.polygon_id ==voronoi_polygon_id ]))
-        df_points_in_polygon["TI_zone_assigend_to_polygon"] = TI_zone_value_all
-        df_polygon_TI_zone = df_points_in_polygon.drop_duplicates(subset ="polygon_id", keep = "last")
-        list_polygon_TI_zone = df_polygon_TI_zone.TI_zone_assigend_to_polygon.tolist() 
+        #append TI_zone taht is most common to polygon that point is in. position 7
+        for i in range(len(points_in_polygon_no_dublicates)):
+            points_in_polygon_no_dublicates[i].append(TI_zone_most_common_all[i])
+
+        # what agent what TI_zone assinged
+        TI_zone_per_agent = []
+        TI_zone_per_agent_set  = set()
+        for point in points_in_polygon_no_dublicates:
+
+            polygon_id = tuple(point)[3]
+
+            if polygon_id not in TI_zone_per_agent_set:
+                TI_zone_per_agent.append(point[6])
+                TI_zone_per_agent_set.add(polygon_id)
+        
         # assign polygons to individual agents
         for i,agent in enumerate(self.active_agents):
-            agent.update_agent_properties(polygon_id = i,polygon = poly_shapes[i],polygon_area = poly_shapes[i].area,TI_zone_assigned = list_polygon_TI_zone[i])
+            agent.update_agent_properties(polygon_id = i,polygon = poly_shapes[i],polygon_area = poly_shapes[i].area,TI_zone_assigned = TI_zone_per_agent[i])
 
-    def missing_elements(self,L):
-        """ check if list of values is throughgoing . return values taht are not """ 
-        start, end = L[0], L[-1]
-        return sorted(set(range(start, end + 1)).difference(L))
+
+###########
+        # # generate df
+        # df_points_in_polygon = pd.DataFrame(points_in_polygon_no_dublicates,columns=["point_x","point_y","point_id","polygon_id","index","TI_zone"])
+
+        # # what TI_zone dominates in which polygon
+        # TI_zone_value_all = []
+        # for voronoi_polygon_id in range(len(poly_shapes)):
+        #     TI_zone_value = df_points_in_polygon[df_points_in_polygon.polygon_id == voronoi_polygon_id]["TI_zone"].value_counts(dropna = False).index
+        #     if len(TI_zone_value) == 0:
+        #         TI_zone_value = TI_zone_value_all[-1]
+        #     else:
+        #         TI_zone_value = TI_zone_value[0].tolist()
+
+        #     TI_zone_value_all.extend([TI_zone_value]*len(df_points_in_polygon[df_points_in_polygon.polygon_id ==voronoi_polygon_id ]))
+
+        # df_points_in_polygon["TI_zone_assigend_to_polygon"] = TI_zone_value_all
+        # df_polygon_TI_zone = df_points_in_polygon.drop_duplicates(subset ="polygon_id", keep = "last")
+        # list_polygon_TI_zone = df_polygon_TI_zone.TI_zone_assigend_to_polygon.tolist()
+
+
+
+         #################       
+        # some points are assigned to multiple polygons. dropping dublicates
+        # df_points_in_polygon = pd.DataFrame(points_in_polygon,columns=["point_x","point_y","point_id","polygon_id"])
+        # df_points_in_polygon.drop_duplicates(subset ="point_id", keep = "last", inplace = True)
+
+        # df_points_in_polygon = df_points_in_polygon.sort_values(by = ["point_id"])
+        # df_points_in_polygon["TI_zone"] = self.env.flatten()
+
+        # df_points_in_polygon = df_points_in_polygon.sort_index()
+
+        # TI_zone_value_all = []
+        # for voronoi_polygon_id in range(len(poly_shapes)):
+        #     TI_zone_value = df_points_in_polygon[df_points_in_polygon.polygon_id == voronoi_polygon_id]["TI_zone"].value_counts(dropna = False).index
+        #     if len(TI_zone_value) == 0:
+        #         TI_zone_value = TI_zone_value_all[-1]
+        #     else:
+        #         TI_zone_value = TI_zone_value[0].tolist()
+
+        #     TI_zone_value_all.extend([TI_zone_value]*len(df_points_in_polygon[df_points_in_polygon.polygon_id ==voronoi_polygon_id ]))
+        # df_points_in_polygon["TI_zone_assigend_to_polygon"] = TI_zone_value_all
+        # df_polygon_TI_zone = df_points_in_polygon.drop_duplicates(subset ="polygon_id", keep = "last")
+        # list_polygon_TI_zone = df_polygon_TI_zone.TI_zone_assigend_to_polygon.tolist() 
+        # # assign polygons to individual agents
+        # for i,agent in enumerate(self.active_agents):
+        #     agent.update_agent_properties(polygon_id = i,polygon = poly_shapes[i],polygon_area = poly_shapes[i].area,TI_zone_assigned = list_polygon_TI_zone[i])
+
+    # def missing_elements(self,L):
+    #     """ check if list of values is throughgoing . return values taht are not """ 
+    #     start, end = L[0], L[-1]
+    #     return sorted(set(range(start, end + 1)).difference(L))
 
     def assign_training_image_to_agent(self):
         """ decide which training image agents and their polygon will get"""
